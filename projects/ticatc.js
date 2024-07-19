cc00e60f6b6f0b0928f8381537a07356c5d3441a
asthajain0 <130166764+asthajain0@users.noreply.github.com>
2024-07-19 14:24:21 +0530

    Update ticatc.js

--- projects/ticatc.js
+++ projects/ticatc.js
@@ -1,37 +1,39 @@
 console.log('welcome')
 let ting=new Audio("ting.mp3");
+let finish=new Audio("gameover.mp3")
 let turn="X";
 let gameover=false;
 const changeturn=()=>{
     return turn=== "X"?"0":"X";
 }
 const win=()=>{
-   let inside=document.getElementsByClassName('.inside')
+   let inside=document.getElementsByClassName('inside')
    let wins = [
-    [0, 1, 2, 5, 5, 0],
-    [3, 4, 5, 5, 15, 0],
-    [6, 7, 8, 5, 25, 0],
-    [0, 3, 6, -5, 15, 90],
-    [1, 4, 7, 5, 15, 90],
-    [2, 5, 8, 15, 15, 90],
-    [0, 4, 8, 5, 15, 45],
-    [2, 4, 6, 5, 15, 135],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 ]
-wins.forEach((e)=>{
-    if((inside[e[0]].innerText===inside[e[1]].innerText) && (inside[e[1]].innerText===inside[e[2]].innerText) && inside[e[0]].innerText!=""){
-        document.querySelector('.result').innerText = inside[e[0]].innerText + " Won"
+wins.forEach(e=>{
+    if((inside[e[0]].innerText===inside[e[1]].innerText) && (inside[e[2]].innerText===inside[e[1]].innerText) && inside[e[0]].innerText!=""){
+        document.querySelector('.out').innerText = inside[e[0]].innerText + " Won"
         gameover=true;
-        
+        finish.play();
     }
 })
+ 
 }
 let box=document.getElementsByClassName("box");
         Array.from(box).forEach(ele=>{
-            let fine=ele.querySelectorAll('.inside');
+            let fine=ele.querySelector('.inside');
             ele.addEventListener('click',()=>{
                 if(fine.innerText===''){
                     fine.innerText=turn;
-                    turn = changeTurn();
+                    turn = changeturn();
                     ting.play();
                     win();
                     if (!gameover){
@@ -40,3 +42,16 @@ let box=document.getElementsByClassName("box");
                 }
             })
         })
+
+        reset.addEventListener('click', ()=>{
+            let boxtexts = document.querySelectorAll('.inside');
+            Array.from(boxtexts).forEach(element => {
+                element.innerText = ""
+            });
+            turn = "X"; 
+            gameover = false
+            
+            document.getElementsByClassName("out")[0].innerText  = "Turn for " + turn;
+            
+        })
+        
